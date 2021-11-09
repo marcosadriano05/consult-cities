084435f514d84e1c89fa2d150d6a511dbeac65d3
Marcos Adriano <marcosadriano740@gmail.com>
2021-11-08 22:34:06 -0300

    add pagination in country route

--- src/main/java/com/github/marcosadriano05/consultcities/countries/presentation/CountryController.java
+++ src/main/java/com/github/marcosadriano05/consultcities/countries/presentation/CountryController.java
@@ -3,11 +3,11 @@ package com.github.marcosadriano05.consultcities.countries.presentation;
 import com.github.marcosadriano05.consultcities.countries.entities.Country;
 import com.github.marcosadriano05.consultcities.countries.repositories.CountryRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
 
 @RestController
 @RequestMapping("/countries")
@@ -17,7 +17,7 @@ public class CountryController {
     private CountryRepository countryRepository;
 
     @GetMapping
-    public List<Country> getAllCountries() {
-        return this.countryRepository.findAll();
+    public Page<Country> getAllCountries(Pageable page) {
+        return this.countryRepository.findAll(page);
     }
 }
