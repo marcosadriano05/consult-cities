8b2d242cbe250d4eb0149aef593f7831f33c7b3a
Marcos Adriano <marcosadriano740@gmail.com>
2021-11-08 22:05:47 -0300

    setup inicial do projeto

---
+++ src/main/java/com/github/marcosadriano05/consultcities/ConsultcitiesApplication.java
@@ -0,0 +1,13 @@
+package com.github.marcosadriano05.consultcities;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ConsultcitiesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ConsultcitiesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/marcosadriano05/consultcities/ConsultcitiesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.marcosadriano05.consultcities;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConsultcitiesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
